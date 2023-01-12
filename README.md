02a4bbee866da1c60f21071edc3c18bc72a60cd2
Adam Grofe <agrofe1417@gmail.com>
2023-01-12 06:56:13 -0800

    Add initial set of tools to the repo

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# C-DevKit
+# C++ DevKit
 This repository is a set of useful scripts or tools for C++ coding. These include cmake scripts for automatically performing static analysis and formatting, for instance.
---
+++ cmake/code-quality.cmake
@@ -0,0 +1,46 @@
+
+
+# Define function to handle running clang-format 
+# over all files
+function(format target directory )
+    find_program(CLANG-FORMAT_PATH clang-format REQUIRED)
+
+    set(EXPRESSION h hpp hh c cc cxx cpp)
+    list(TRANSFORM EXPRESSION PREPEND "${directory}/*.")
+
+    file(GLOB_RECURSE SOURCE_FILES FOLLOW_SYMLINKS LIST_DIRECTORIES false ${EXPRESSION})
+
+    add_custom_command(TARGET ${target} PRE_BUILD 
+        COMMAND ${CLANG-FORMAT_PATH} -i --style=file:${CMAKE_SOURCE_DIR}/clang-format ${SOURCE_FILES}
+    )
+endfunction()
+
+# Set up cppcheck static analysis
+find_program(CPPCHECK_PATH cppcheck REQUIRED)
+set(CMAKE_CXX_CPPCHECK ${CPPCHECK_PATH})
+
+# Define function to handle running flawfinder (security analysis)
+# over all files
+function(flawfinder target directory )
+    find_program(FLAWFINDER_PATH flawfinder REQUIRED)
+
+    set(EXPRESSION h hpp hh c cc cxx cpp)
+    list(TRANSFORM EXPRESSION PREPEND "${directory}/*.")
+
+    file(GLOB_RECURSE SOURCE_FILES FOLLOW_SYMLINKS LIST_DIRECTORIES false ${EXPRESSION})
+
+    add_custom_command(TARGET ${target} PRE_BUILD 
+        COMMAND ${FLAWFINDER_PATH} ${SOURCE_FILES}
+    )
+endfunction()
+
+
+# Run all of the code quality analyses
+function(run_code_quality_analysis target directory)
+    format(${target} ${directory})
+    flawfinder(${target} ${directory})
+endfunction()
+
+# Change Default CXX Flags for Debug
+add_compile_options(-fsanitize=address -Wall -Wextra -Wpedantic)
+add_link_options(-fsanitize=address -Wall -Wextra -Wpedantic)
